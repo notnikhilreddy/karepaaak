12331385331c86b4098912a8588316789792a8e7
nikhilreddy007 <nikhilreddy.maligireddy1@gmail.com>
2020-03-30 09:29:42 +0530

    added enemy

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public string enemyType;
+    public float[] movePoints = new float[2];
+    private int speed;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter2D(Collider2D trig)
+    {
+
+    }
+
+
+}
+
+
