7f1c57aaccc9755ec795ce654c562de09ce77885
nikhilreddy007 <nikhilreddy.maligireddy1@gmail.com>
2020-04-02 15:39:46 +0530

    player health bar added

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -8,11 +8,9 @@ public class EnemyHealth : MonoBehaviour
     // Start is called before the first frame update
     public float enemyMaxHealth;
     public Slider enemySlider;
-    private Rigidbody2D rb;
     private float currentHealth;
     void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody2D>();
         currentHealth = enemyMaxHealth;
         enemySlider.gameObject.SetActive(false);
         enemySlider.maxValue = currentHealth;
@@ -22,16 +20,17 @@ public class EnemyHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(transform.localScale.x >= 0f) {
+            enemySlider.SetDirection(Slider.Direction.LeftToRight, false);
+        } else {
+            enemySlider.SetDirection(Slider.Direction.RightToLeft, false);
+        }
         if (currentHealth <= 0) makeDead();
     }
 
     public void addDamage(float damage)
     {
-        if(rb.velocity.x >= 0f) {
-            enemySlider.SetDirection(Slider.Direction.LeftToRight, false);
-        } else {
-            enemySlider.SetDirection(Slider.Direction.RightToLeft, false);
-        }
+        if (damage <= 0) return;
         enemySlider.gameObject.SetActive(true);
         currentHealth -= damage;
         enemySlider.value = currentHealth;
