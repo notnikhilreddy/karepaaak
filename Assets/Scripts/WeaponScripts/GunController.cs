f5541270311e0432e95a0fba706c1049bc02b4f6
nikhilreddy007 <nikhilreddy.maligireddy1@gmail.com>
2020-04-07 15:58:46 +0530

    split weapon controller code

--- Assets/Scripts/WeaponScripts/GunController.cs
+++ Assets/Scripts/WeaponScripts/GunController.cs
@@ -2,46 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponController : MonoBehaviour
+public class GunController : MonoBehaviour
 {
-    public enum WeaponTypes {
-        Gun,
-        Melee,
-        Mouth,
-        Plasma
-    }
-
-    public WeaponTypes weaponType;
     public float bulletSpeed, fireDelay, bulletsPerFire;
     public bool simultaneously;
     public float bulletSpreadingAngle, timeBetweenBullets, damagePerFire;
     public float visualRange, visualAngle;
     public GameObject bulletObject;
     public LineRenderer aimingLaser;
-    
     private GameObject parentObject;
     private string weaponOwner;
     private Vector2 pointDirection, firePoint;
     private Transform parentTransform;
-    private IEnumerator coroutine;
+
     // Start is called before the first frame update
-    
-    void Start() {
+    void Start()
+    {
         parentObject = transform.parent.gameObject;
         parentTransform = parentObject.transform;
 
         weaponOwner = parentObject.tag;
 
-        string weapon = weaponType.ToString();
-
-        if(weapon.Equals("Gun"))
-            coroutine = Gun();
-        else if(weapon.Equals("Melee"))
-            coroutine = Melee();
-        else if(weapon.Equals("Mouth"))
-            coroutine = Mouth();
-        else if(weapon.Equals("Plasma"))
-            coroutine = Plasma();
+        IEnumerator coroutine = Gun();
 
         StartCoroutine(coroutine);
     }
@@ -171,130 +153,4 @@ public class WeaponController : MonoBehaviour
         }
     }
 
-    private IEnumerator Melee() {
-        if(weaponOwner.Equals("Player")) {
-            float lastFireTime = 0f;
-            while(true) {
-                if(Input.GetMouseButtonDown(0)) {
-                    
-                    if(Time.time - lastFireTime >= fireDelay) {
-
-                        firePoint = transform.GetChild(0).position;
-                        GameObject newBullet = Instantiate(bulletObject, firePoint, transform.rotation, transform);
-                        newBullet.GetComponent<BulletController>().shotBy = parentObject.tag;
-                        newBullet.GetComponent<BulletController>().damage = damagePerFire;
-
-                        lastFireTime = Time.time;
-
-                        float time = Time.time;
-                        while(Time.time - time <= 0.5f) {
-                            yield return null;
-                        }
-
-                        Destroy(newBullet);
-                    }
-                }
-
-                yield return null;
-            }
-        } else if(weaponOwner.Equals("Enemy")) {
-            Vector2 raycastDirection;
-            // RaycastHit2D hitInfo, hit;
-            float lastFireTime = Time.time;
-            
-            while(true) {
-                bool playerSpotted = false;
-
-                RaycastHit2D hitInfo, hit;
-
-                raycastDirection = parentTransform.right * parentTransform.localScale.x;
-                raycastDirection = raycastDirection.normalized;
-                
-                hitInfo = Physics2D.Raycast(transform.position, raycastDirection, 0, ~1 << LayerMask.NameToLayer("Enemies"));
-                for(float angle = visualAngle/2f; angle >= -visualAngle/2f; angle -= visualAngle/(visualRange*2)) {
-                    hit = Physics2D.Raycast(transform.position, Quaternion.Euler(0f, 0f, angle) * raycastDirection, visualRange, ~1 << LayerMask.NameToLayer("Enemies"));
-                    
-                    if(hit && hit.collider.tag.Equals("Player")) {
-                        hitInfo = hit;
-                    }
-                }
-                
-                if(hitInfo && hitInfo.collider.tag.Equals("Player")) {
-                    playerSpotted = true;
-
-                    Vector3 hitPoint = hitInfo.collider.transform.position;
-
-                    pointDirection = hitPoint - parentObject.transform.position;
-                    pointDirection = pointDirection.normalized;
-
-                    Vector2 runVelocity;
-
-                    parentObject.GetComponent<EnemyController>().isAttacking = true;
-
-                    if(pointDirection.x >= 0) {
-                        runVelocity = new Vector2(parentObject.GetComponent<EnemyController>().speed, parentObject.GetComponent<Rigidbody2D>().velocity.y);
-                    } else {
-                        runVelocity = new Vector2(-parentObject.GetComponent<EnemyController>().speed, parentObject.GetComponent<Rigidbody2D>().velocity.y);
-                    }
-                    
-                    parentObject.GetComponent<Rigidbody2D>().velocity = runVelocity;
-                    
-                    if(hitInfo.distance <= 1.5f) {
-                        parentObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-                        if(Time.time - lastFireTime >= fireDelay) {
-                            
-                            firePoint = transform.GetChild(0).position;
-                            GameObject newBullet = Instantiate(bulletObject, firePoint, transform.rotation, transform);
-                            newBullet.GetComponent<BulletController>().shotBy = parentObject.tag;
-                            newBullet.GetComponent<BulletController>().damage = damagePerFire;
-
-                            lastFireTime = Time.time;
-
-                            float time = Time.time;
-                            while(Time.time - time <= 0.5f) {
-                                yield return null;
-                            }
-
-                            Destroy(newBullet);
-                        }
-                    }
-                }
-
-                if(!playerSpotted) {
-                    parentObject.GetComponent<EnemyController>().isAttacking = false;
-                    transform.localRotation = Quaternion.Euler(0, 0, 0);
-                }
-
-                yield return null;
-            }
-        }
-    }
-
-    private IEnumerator Mouth() {
-        if(weaponOwner.Equals("Player")) {
-            while(true) {
-
-                yield return null;
-            }
-        } else if(weaponOwner.Equals("Enemy")) {
-            while(true) {
-
-                yield return null;
-            }
-        }
-    }
-
-    private IEnumerator Plasma() {
-        if(weaponOwner.Equals("Player")) {
-            while(true) {
-
-                yield return null;
-            }
-        } else if(weaponOwner.Equals("Enemy")) {
-            while(true) {
-
-                yield return null;
-            }
-        }
-    }
 }
---
+++ Assets/Scripts/WeaponScripts/PlasmaGunController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlasmaGunController : MonoBehaviour
+{
+    private GameObject parentObject;
+    private string weaponOwner;
+    private Vector2 pointDirection, firePoint;
+    private Transform parentTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parentObject = transform.parent.gameObject;
+        parentTransform = parentObject.transform;
+
+        weaponOwner = parentObject.tag;
+
+        IEnumerator coroutine = Plasma();
+
+        StartCoroutine(coroutine);
+    }
+
+    private IEnumerator Plasma() {
+        if(weaponOwner.Equals("Player")) {
+            while(true) {
+
+                yield return null;
+            }
+        } else if(weaponOwner.Equals("Enemy")) {
+            while(true) {
+
+                yield return null;
+            }
+        }
+    }
+}
